162f0fb77c35ec82f1c035a4a5170578e6b53773
rodrigoferreiramilioli <soares_digo@hotmail.com>
2020-11-10 23:51:37 -0300

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
-import People from "./components/People";
+import "./components/Personagem/Personagem.scss";
+import Personagem from "./components/Personagem/Personagem";
 
-export default function App() {
-  return (
-    <div>
-      <h1>People</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      personagens: [],
+      next: "",
+      previous: ""
+    };
+
+    this.updateData = this.updateData.bind(this);
+  }
+
+  updateData(data) {
+    this.setState({
+      personagens: [],
+      next: data.next,
+      previous: data.previous
+    });
+  }
+  async componentDidMount() {
+    const response = await Personagem.get("");
+    this.setState({
+      personagens: response.data.results,
+      next: response.data.next,
+      previous: response.data.previous
+    });
+  }
+  render() {
+    const { personagens } = this.state;
+    return (
+      <div>
+        <h1>Listar Personagens</h1>
+        <table border="1">
+          <tr>
+            <th>Nome</th>
+            <th>Olhos</th>
+          </tr>
+          {personagens.map((personagem, i) => (
+            <tr>
+              <td key={i} infos={personagem.name} type={"personagem"}>
+                <label class="Personagem">{personagem.name}</label>
+              </td>
+              <td key={i} infos={personagem.eye_color} type={"personagem"}>
+                <label class="Personagem">{personagem.eye_color}</label>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
 }
+
+export default App;
--- src/components/Personagem/Personagem.js
+++ src/components/Personagem/Personagem.js
@@ -1,62 +1,7 @@
 import axios from "axios";
-class Personagem extends Component {
-  constructor(props) {
-    super(props);
+import React, { Component } from "react";
 
-    this.state = {
-      personagens: [],
-      next: "",
-      previous: ""
-    };
-
-    this.updateData = this.updateData.bind(this);
-  }
-
-  updateData(data) {
-    this.setState({
-      personagens: data.results,
-      next: data.next,
-      previous: data.previous
-    });
-  }
-
-  getPersonagem() {
-    return axios
-      .get("https://swapi.co/api/people/")
-      .then(response => {
-        this.setState({
-          personagens: response.data.results,
-          next: response.data.next,
-          previous: response.data.previous
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  componentDidMount() {
-    this.getPersonagem();
-  }
-
-  render() {
-    const { personagens, next, previous } = this.state;
-
-    return (
-      <div className="People">
-        <div className="People-grid">
-          {people.map((personagens, i) => (
-            <Card key={i} infos={personagens} type={"personagens"} />
-          ))}
-        </div>
-        <NextAndPrevious
-          updateData={this.updateData}
-          next={next}
-          previous={previous}
-        />
-      </div>
-    );
-  }
-}
-
+const Personagem = axios.create({
+  baseURL: "https://swapi.dev/api/people/"
+});
 export default Personagem;
